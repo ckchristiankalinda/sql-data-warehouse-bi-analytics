52c6b0d6ca6298a58579494b8e99c4034ed4736b
MUTIA KALINDA CHRISTIAN <ckchristiankalinda5@gmail.com>
2026-05-26 13:22:24 +0300

    Update erp_transformation.sql

--- 02_data_cleaning/erp_transformation.sql
+++ 02_data_cleaning/erp_transformation.sql
@@ -47,3 +47,71 @@ SELECT
     END AS gen
 
 FROM bronze.erp_cust_az12;
+
+/*=========================================================
+Load ERP Location Data from Bronze to Silver Layer
+Target Table: silver.erp_loc_a101
+
+Transformation Types:
+1. Data Cleansing
+   - Remove special characters from customer IDs.
+2. Data Standardization
+   - Convert country abbreviations into full country names.
+3. Missing Data Handling
+   - Replace null or empty country values with 'n/a'.
+=========================================================*/
+
+INSERT INTO silver.erp_loc_a101(
+    cid,
+    cntry
+)
+
+SELECT 
+
+-- Remove hyphens from customer ID
+REPLACE(cid,'-',''),
+
+-- Standardize country names
+CASE 
+    WHEN TRIM(cntry)= 'DE' 
+        THEN 'Germany'
+
+    WHEN TRIM(cntry) IN ('US','USA') 
+        THEN 'United States'
+
+    -- Handle missing values
+    WHEN TRIM(cntry)='' 
+         OR cntry IS NULL 
+        THEN 'n/a'
+
+    ELSE TRIM(cntry)
+
+END AS cntry
+
+FROM bronze.erp_loc_a101;
+
+/*=========================================================
+Load Product Category Data from Bronze to Silver Layer
+Target Table: silver.erp_px_cat_g1v2
+
+Transformation Types:
+1. Direct Data Loading
+   - No transformation applied.
+2. Data Transfer
+   - Data moved as-is from Bronze to Silver.
+=========================================================*/
+
+INSERT INTO silver.erp_px_cat_g1v2(
+    id,
+    cat,
+    subcat,
+    maintenance
+)
+
+SELECT 
+    id,
+    cat,
+    subcat,
+    maintenance
+
+FROM bronze.erp_px_cat_g1v2;
